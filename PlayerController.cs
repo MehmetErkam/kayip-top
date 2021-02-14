8a9e857a5d038d77eb076919a8c88045aae0f126
MehmetErkam <62347408+MehmetErkam@users.noreply.github.com>
2021-02-14 18:42:05 +0300

    Oyuncu kontrolcü Scripti
    Bu C# scripti, yüklenen objeye;  klavye,  gamepad, ekran joystick'i  ve tilt sensörü ile kontrol desteğini sağlar.

---
+++ PlayerController.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool duzMu = true; // Cihazın düzlemini kontrol etme
+    public FixedJoystick variableJoystick; // Mobil Joysick için değişken
+    private Rigidbody rigid; // rigidbody değişkeni
+    public bool gyroscope = false; // Gyroscope Sensorü anahtarı
+    public bool keyboard = true; // Klavye kontrolü anahtarı
+    public bool joystick=true; // Joystick kontrolü anahtarı
+    public int hiz = 10; // hız değişkeni
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>(); 
+        // Scriptin bulunduğu değişkenin rigidbody'si
+    }
+
+    void FixedUpdate()
+    {
+        // gyroscope sensöründen elde edilen veriyi input olarak alıp
+        // rigidbody ye velocity değeri olarak atar.
+
+        if (gyroscope)
+        {
+            Vector3 egim = -Input.acceleration;
+
+            if (duzMu)
+                egim = Quaternion.Euler(90, 0, 180) * egim;
+
+            rigid.velocity = egim * hiz;
+        }
+
+        // klavye kontrollerinin ve gamepad kontrollerinin gerçekleşmesini
+        // sağlar. Yatay ve dikey yönde alınan girdiler, x ve z düzleminde
+        // hareket için addforce fonksiyonuna parametre olarak verilir. 
+
+        if (keyboard)
+        {
+            float xMov = Input.GetAxisRaw("Horizontal")*hiz;
+            float zMov = Input.GetAxisRaw("Vertical")*hiz;
+
+            rigid.AddForce(xMov,0,zMov);
+        }
+
+        // Mobil ekranlarda hareket için sanal bir analog hareket sağlanabilir.
+        // Ekranda gözüken analog'un girilen yön doğrultusunda hareketi sağlaması
+        // için Vector3 sınıfında direction değişkeni oluşturulur. bu değişkende 
+        // vector3'ün ileri fonksiyonu ve sağ fonksiyonu toplanır. toplanmadan önce
+        // ileri => Vertical ile, Sağ => Horizontal girdi ile çarpılır. AddForce ile 
+        // rigidbody ye yön, hız, sabit zaman ve hız değişkenliğini  kontrol etme
+        // özellikleri kazandırılır.
+
+        if (joystick)
+        {
+            Vector3 direction = Vector3.forward * 
+            variableJoystick.Vertical + Vector3.right * 
+            variableJoystick.Horizontal;
+            
+            rigid.AddForce(direction * 
+            hiz * Time.fixedDeltaTime, 
+            ForceMode.VelocityChange);
+        }
+    }
+}
